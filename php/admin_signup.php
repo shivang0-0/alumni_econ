ced27f3851d31ab26c56d2f61357fa642be026b8
root <root@pop-os.localdomain>
2022-11-18 18:08:53 +0530

    introduced more tables

--- php/admin_signup.php
+++ php/admin_signup.php
@@ -3,13 +3,15 @@ $con = mysqli_connect("localhost","root","","project");
 $aname = $_POST['nm'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
+$desig = $_POST['desig'];
 if(!$con)
 {
     echo "Failed to connect to MYSQL: " . mysqli_connect_error();
 }
 $sql1 = "INSERT INTO admin_details VALUES (NULL , '$aname' , '$email')";
 $sql2 = "INSERT INTO admin_cred VALUES ('$email','$pass')";
-if(mysqli_query($con,$sql1) && mysqli_query($con,$sql2))
+$sql3 = "INSERT INTO admin_dash VALUES (NULL, '$desig')";
+if(mysqli_query($con,$sql1) && mysqli_query($con,$sql2) && mysqli_query($con,$sql3))
 {
     echo "<script>";
     echo"alert('Admin Account Created Successfully!');";
--- php/company_signup.php
+++ php/company_signup.php
@@ -3,12 +3,16 @@ $con = mysqli_connect("localhost","root","","project");
 $cname = $_POST['nm'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
+$location_based = $_POST['location_based'];
+$phone = $_POST['phone'];
+$website = $_POST['website'];
 if(!$con)
 {
     echo "Failed to connect to MYSQL: " . mysqli_connect_error();
 }
 $sql1 = "INSERT INTO company_details VALUES (NULL , '$cname' , '$email')";
 $sql2 = "INSERT INTO company_cred VALUES ('$email','$pass')";
+$sql3 = "INSERT INTO company_dash VALUES (NULL, '$location_based', '$phone', '$website')";
 
 if(mysqli_query($con,$sql1) && mysqli_query($con,$sql2))
 {
@@ -18,4 +22,4 @@ if(mysqli_query($con,$sql1) && mysqli_query($con,$sql2))
     echo"</script>";
 }
 mysqli_close($con);
-?>
+?>website
